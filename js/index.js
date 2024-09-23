7ce737fea38ece6107eb1a9fee3c9f348437a066
Yead <yead19@gmail.com>
2024-09-23 16:32:29 +0600

    ntm

--- js/index.js
+++ js/index.js
@@ -26,44 +26,55 @@ calculatebtn.addEventListener('click', function () {
     const result = document.getElementById('results')
     result.classList.remove('hidden')
 
-  
 
-
-
-
-    const calSavings = document.getElementById('calculate-savings')
-    calSavings.addEventListener('click', function () {
-        const savingsInput = document.getElementById('savings').value
-
-        const totalSavings = balance * savingsInput / 100
-        const savingsAmount = document.getElementById('savings-amount')
-        savingsAmount.innerText = totalSavings
-
-        const finalBalance = balance - totalSavings
-        const remainingBalance = document.getElementById('remaining-balance')
-        remainingBalance.innerText = finalBalance
-
-
-    })
-
-      // history item
-
-      const historyItem = document.createElement('div')
-      historyItem.className = "bg-white p-3 rounded-md border-l-2 border-indigo-500";
-      historyItem.innerHTML = `
-  <p class="text-xs text-gray-500">${new Date().toLocaleDateString()} </p>
-  <p class="text-xs text-gray-500">Income: ৳${income.toFixed(2)} </p> 
-  <p class="text-xs text-gray-500">Income: ৳${totalExpenses.toFixed(2)} </p> 
-  <p class="text-xs text-gray-500">Income: ৳${finalBalance.toFixed(2)} </p> 
-  `
-  
-      const historyContainer = document.getElementById('history-list')
-      historyContainer.insertBefore(historyItem, historyContainer.firstChild)
-
-    
+    // history item
+    const historyItem = document.createElement("div")
+    historyItem.className = 'bg-white p-3 rounded-md border-l-2 border-indigo 500'
+    historyItem.innerHTML = `
+    <p class= "text-xs text-gray-500 font-bold">Serial: ${count}</p>
+    <p class= "text-xs text-gray-500">${new Date().toLocaleDateString() }</p>
+    <p class= "text-xs text-gray-500">Income: $${income.toFixed(2)}</p>
+    <p class= "text-xs text-gray-500">Expense: $${totalExpenses.toFixed(2)}</p>
+    <p class= "text-xs text-gray-500">Balance: $${balance.toFixed(2)}</p>
+    `
+    const historyContainer = document.getElementById('history-list')
+    historyContainer.insertBefore(historyItem, historyContainer.firstChild)
 
 
 })
+
+
+// savings
+const calSavings = document.getElementById('calculate-savings')
+calSavings.addEventListener('click', function () {
+    const income = parseFloat(document.getElementById('income').value);
+    const software = parseFloat(document.getElementById('software').value);
+    const courses = parseFloat(document.getElementById('courses').value);
+    const internet = parseFloat(document.getElementById('internet').value);
+
+    const totalExpenses = software + courses + internet;
+    const balance = income - totalExpenses
+
+
+    const resExpenses = document.getElementById('total-expenses')
+    resExpenses.innerText = totalExpenses
+    const resBalance = document.getElementById('balance')
+    resBalance.innerText = balance
+
+
+    const savingsInput = document.getElementById('savings').value
+
+    const totalSavings = balance * savingsInput / 100
+    const savingsAmount = document.getElementById('savings-amount')
+    savingsAmount.innerText = totalSavings.toFixed(2)
+
+    const finalBalance = balance - totalSavings
+    const remainingBalance = document.getElementById('remaining-balance')
+    remainingBalance.innerText = finalBalance.toFixed(2)
+
+
+})
+
 
 
 
@@ -81,15 +92,18 @@ history.addEventListener('click', function () {
 
     document.getElementById('expense-form').classList.add('hidden')
 
-    // document.getElementById('history-section').classList.remove('hidden')
-    document.getElementById('results').classList.remove('hidden')
+    document.getElementById('history-section').classList.remove('hidden')
+    document.getElementById('results').classList.remove('hidden') 
 
 
 
+})
 
-
-
-
-
+assistant.addEventListener('click', function(){
+    assistant.classList.add('text-white', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600','text-gray-600')
+    history.classList.remove('text-white', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600')
+    document.getElementById('expense-form').classList.remove('hidden')
+    document.getElementById('history-section').classList.add('hidden')
+    document.getElementById('results').classList.add('hidden') 
 
 })
